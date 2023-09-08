4ab7f2388c268fee26671232034f1325008956c5
최근원 <kidjustinchoi@gmail.com>
2023-09-08 11:19:08 +0900

    feat: add logo container in main page

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { Logo } from '@/assets';
+
+import * as S from './styled';
+
 export const MainPage: React.FC = () => {
   return (
-    <div>
-      <h1>Main Page</h1>
-    </div>
+    <S.MainPageContainer>
+      <S.MainPageLogoContainer>
+        <S.MainPageLogo src={Logo} alt="logo" />
+        <S.MainPageText>한움페이 부스운영 시스템</S.MainPageText>
+      </S.MainPageLogoContainer>
+    </S.MainPageContainer>
   );
 };
---
+++ src/pages/main/styled.ts
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+export const MainPageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MainPageLogoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MainPageLogo = styled.img`
+  width: 170px;
+  height: 50px;
+`;
+
+export const MainPageText = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+`;
